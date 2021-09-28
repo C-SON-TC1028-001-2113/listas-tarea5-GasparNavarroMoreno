6666df76789a28ac26ea62c19f1591e1444d7ac5
GasparNavarroMoreno <a01252422@itesm.mx>
2021-09-28 06:21:33 +0000

    tarea5Intento1

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,8 +1,15 @@
-
-
-def main():
-
-
+def main(): 
+    x = int(input(''))
+    y = int(input(''))
+    z = x*y
+    lista=[]
+    i=1
+    while i<=z:
+        i=i+1
+        valor = int(input(''))
+        if valor<10:
+            lista.append(valor)
+    print(lista)
 
 
 if __name__=='__main__':
